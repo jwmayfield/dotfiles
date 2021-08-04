90dc1d83b8ae39a9d281d2ddcbce42d2a6fb523e
Jason Mayfield <jason@jwmayfield.com>
2021-08-04 10:52:24 -0400

    Ignore any fish config files added by plugins, etc., and fix homebrew sourcing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
 set -g fish_key_bindings fish_vi_key_bindings
 set -g HOMEBREW_PREFIX /opt/homebrew
-set -g XDG_CONFIG_HOME -$HOME/.config
+set -g XDG_CONFIG_HOME $HOME/.config
 
+eval (/opt/homebrew/bin/brew shellenv)
 starship init fish | source
 
 source $XDG_CONFIG_HOME/fish/alias.fish
