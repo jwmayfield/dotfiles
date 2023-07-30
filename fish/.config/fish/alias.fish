181347c94fe1be4325bdc89f8157171eeaa2479d
Jason Mayfield <jason@jwmayfield.com>
2023-07-30 11:00:31 -0400

    Various updates, including shifting back to nvim using LunarVim / nvim-basic-ide

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -2,6 +2,7 @@ alias cat=bat
 alias cloc=tokei
 alias find=fd
 alias grep=rg
+alias hawk='~/Code/hawk-3.1.0/hawk'
 alias ls='exa -la'
 alias ps=procs
 alias rm='rm -i'
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,10 +6,6 @@ eval (/opt/homebrew/bin/brew shellenv)
 starship init fish | source
 
 source $XDG_CONFIG_HOME/fish/alias.fish
-source $HOMEBREW_PREFIX/opt/asdf/asdf.fish
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
-# Platform.sh config
-fish_add_path $HOME/.platformsh/bin
-if test -f $HOME/.platformsh/shell-config.rc
-	$HOME/.platformsh/shell-config.rc
-end
+fish_add_path /opt/homebrew/opt/openjdk/bin
