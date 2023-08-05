d60e55e0a55f360ba0d57201d380c146408452c6
Jason Mayfield <jason@jwmayfield.com>
2023-08-05 17:12:07 -0400

    More config for LSPs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,34 +1,64 @@
 return {
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        config = function()
-            local lsp = require('lsp-zero').preset("recommended")
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    config = function()
+      local lsp = require("lsp-zero").preset("recommended")
 
-            lsp.on_attach(function(client, bufnr)
-                lsp.default_keymaps({ buffer = bufnr })
-                lsp.buffer_autoformat()
-            end)
+      lsp.on_attach(function(client, bufnr)
+        lsp.default_keymaps({ buffer = bufnr })
+        lsp.buffer_autoformat()
+      end)
 
-            lsp.ensure_installed({
-                'lua_ls',
-                'standardrb',
-            })
+      lsp.ensure_installed({
+        "html",
+        "lua_ls",
+        "standardrb",
+      })
 
-            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-            lsp.setup()
-        end,
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
+      require("lspconfig").html.setup({
+        filetypes = { "html", "eruby" },
+        init_options = {
+          provideFormatter = false,
         },
+      })
+      require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+
+      lsp.setup()
+    end,
+    dependencies = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "L3MON4D3/LuaSnip" },
     },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      local formatting = null_ls.builtins.formatting
+      local diagnostics = null_ls.builtins.diagnostics
+
+      null_ls.setup({
+        debug = true,
+        sources = {
+          diagnostics.erb_lint,
+          formatting.prettier,
+          formatting.htmlbeautifier, -- eruby only
+          formatting.rustywind.with({
+            extra_filetypes = { "eruby" },
+          }),
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
