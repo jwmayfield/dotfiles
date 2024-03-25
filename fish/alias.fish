498749e301d911cda241d7bb6d60f497b6844662
Jason Mayfield <jason@jwmayfield.com>
2024-03-25 07:44:15 -0400

    Remove alias ps-procs

--- fish/alias.fish
+++ fish/alias.fish
@@ -4,7 +4,6 @@ alias find=fd
 alias grep=rg
 alias hawk='~/Code/hawk-3.1.0/hawk'
 alias ls='exa -la'
-alias ps=procs
 alias rm='rm -i'
 alias ssh="TERM=xterm-256color $(which ssh)"
 alias tree='broot'
