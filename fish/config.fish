49333963ab0f71aab85abd6774343c3ee4b88ee7
Jason Mayfield <jason@jwmayfield.com>
2026-08-28 15:58:31 +0200

    switch from ASDF to Mise

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,14 @@ eval (/opt/homebrew/bin/brew shellenv)
 starship init fish | source
 
 source $XDG_CONFIG_HOME/fish/alias.fish
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 fish_add_path /opt/homebrew/opt/openjdk/bin
 fish_config theme choose Nord
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+export PATH="$HOME/.local/bin:$PATH"
+
+# groundcrew: mise must run after all other PATH edits
+/Users/jason.mayfield/.local/bin/mise activate fish | source
